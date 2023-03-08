a9d0d8ed4581172d303a6ec9a7c139aed5589f60
besskudnii <besskudnii@mail.ru>
2023-03-08 23:50:10 +0300

    test1

--- 1.cpp
+++ 1.cpp
@@ -1,21 +1,30 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
+#include <string>
 #include <vector>
 
+std::vector<std::string> ReverseVector(std::string& example)
+{
+
+}
 int main()
 {
 	std::string word;
 	std::vector<std::string> words;
-	while (word != "/0")
+	while (std::getline(std::cin, word))
 	{
-		std::cin >> word;
-		words.push_back(word);
+	words.push_back(word);
+	if (words.back() == "") break;
 	}
-	
+	words.pop_back();
+	//int i = 1;
 	for (std::string elem : words)
 	{
+		
 		std::cout << elem << std::endl;
 	}
 	//std::cout << ;
 
 	return 0;
 }
+
