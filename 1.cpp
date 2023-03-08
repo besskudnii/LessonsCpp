d0910679ed9cf913ccd3da18b37ace02af850272
besskudnii <besskudnii@mail.ru>
2023-03-08 21:08:22 +0300

    1

--- 1.cpp
+++ 1.cpp
@@ -3,26 +3,19 @@
 
 int main()
 {
-	size_t count, i = 1;
-	size_t x;
-	std::vector<int> guest, permutation, seats;
-	std::cin >> count;
-	while (guest.size() < count)
+	std::string word;
+	std::vector<std::string> words;
+	while (word != "/0")
 	{
-		std::cin >> x;
-		guest.push_back(x);
-		seats.push_back(i++);
+		std::cin >> word;
+		words.push_back(word);
 	}
-	permutation = guest;
-	for (int i = 0; i < guest.size(); ++i)
+	
+	for (std::string elem : words)
 	{
-		permutation[guest[i] - 1] = seats[i];
+		std::cout << elem << std::endl;
 	}
-	for (int elem : permutation)
-	{
-		std::cout << elem << " ";
-	}
-	std::cout << std::endl;
+	//std::cout << ;
 
 	return 0;
 }
