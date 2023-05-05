dc1ec57c637a7c6491cf89eede19ec971a877912
besskudnii <besskudnii@mail.ru>
2023-05-05 18:17:34 +0300

    print error2

--- Print/Print.cpp
+++ Print/Print.cpp
@@ -25,43 +25,21 @@ void Print(const T& container, const std::string& delimiter)
     {
         std::cout << "";
     }
-        auto it = std::begin(container);
-    if (it != std::end(container))
+    for (auto it = container.begin(); it != container.end(); ++it)
     {
         std::cout << *it;
-        ++it;
-        for (auto it = std::begin(container); it != std::end(container); ++it)
+        if (it < container.end()-1)
         {
-            std::cout << delimiter << *it;
+            std::cout << delimiter;
         }
-
     }
     std::cout << std::endl;
 }
-
-//template <typename T>
-//void Print(const std::vector<T>& temp, const std::string& delimetr)
-//{
-//    for (auto i : temp)
-//    {
-//        std::cout << i << delimetr;
-//    }
-//}
-
-//
-//template <typename T>
-//void Print(const std::array<T, size_t &Size> &temp, const std::string& delimetr)
-//{
-//    for (auto i : temp)
-//    {
-//        std::cout << i << delimetr;
-//    }
-//    std::cout << std::endl;
-//}
+ 
 
 int main()
 {
-    std::vector<int> first = { 5, 1 , 7 ,7};
+    std::vector<int> first = { 5, 1, 7, 7};
     Print(first, ", ");
 
     std::array<int, 2> second = { 5, 1 };
